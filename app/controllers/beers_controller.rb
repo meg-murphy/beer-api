2a3c6ab5cdba682a34be5abe5f6a53a3a77109e1
meg-murphy <meghan.murphy.usc@gmail.com>
2016-12-08 20:38:53 -0800

    add create, update, destroy methods

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -10,6 +10,26 @@ class BeersController < ApplicationController
 
   def create
     @beer = Beer.create(name: params[:name], style: params[:style], hop: params[:hop],
-     yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol params[:alcohol])
+     yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol: params[:alcohol])
     render :show
+  end
+
+  def update
+    @beer = Beer.find_by(id: params[:id])
+    @beer.assign_attributes(name: params[:name], style: params[:style], hop: params[:hop],
+    yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol: params[:alcohol])
+    @beer.save
+    render :show
+    # @beer.update_attributes(name: params[:name], style: params[:style], hop: params[:hop],
+    # yeast: params[:yeast], malts: params[:malts], ibu: params[:ibu], alcohol: params[:alcohol])
+    # # or use update_attributes so you dont have to save it
+  end
+
+  def destroy
+    # @beer = Beer.find_by(id: params[:id])
+    # or use 'find' which finds by id
+    @beer = Beer.find(params[:id])
+    @beer.destroy
+    redirect_to :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/beer", to: "beers#index"
-  get "/beer/:id", to: "beers#show"
+  get '/beer', to: 'beers#index'
+  get '/beer/:id', to: 'beers#show'
 
-  post "/beer", to: "beer#create"
+  post '/beer', to: 'beers#create'
+
+  patch '/beer/:id', to: 'beers#update'
+
+  delete '/beer/:id', to: 'beers#destroy'
+
 
 end
